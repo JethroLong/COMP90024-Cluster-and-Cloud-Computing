3bf0438dde6a1d3c0f37901a2e19d2fe5c093450
jethrolong <longj2@student.unimelb.edu.au>
2019-04-02 18:58:11 +1100

    version-2

--- Assignment 1/src/ass1.py
+++ Assignment 1/src/ass1.py
@@ -1,20 +1,9 @@
-'''
-import libraries
-'''
 from mpi4py import MPI
 import json
+import math
 import sys
 
 
-def get_FileName(argv):
-    if len(argv) > 1:
-        return argv[1]
-    else:
-        return "bigTwitter.json"
-
-
-# load the grid data, specifically the coordinates of vetices
-# of each grid box
 def get_Grid(gridFile):
     with open(gridFile, 'r') as melbGrid:
         gridData = json.load(melbGrid)
@@ -24,25 +13,46 @@ def get_Grid(gridFile):
     return grid
 
 
-# count the total number of data records in a json lines file
-def count_lines(tweet_file):
-    count = 0
-    for line in tweet_file:
-        count += 1
-    return count
+def doOperation_on_tweets(tweet_file, grid):
+    grid_cor_dict = {}
+    grid_hashtag_dict = {}
+
+    for each in tweet_file:
+        # lookup coordinates
+        temp_cor_list = each["value"]["geometry"]["coordinates"]
+        area = which_grid_box(temp_cor_list[0], temp_cor_list[1], grid)
+        # print(area," with coordinates: ", temp_cor_list)
+        if area not in grid_cor_dict.keys():
+            if area is not None:
+                grid_cor_dict[area] = 1
+        else:
+            grid_cor_dict[area] += 1
+
+        # lookup hashtags
+        #####
+
+    return grid_cor_dict, grid_hashtag_dict
 
 
-def order_num_tweets():
-    pass
+def get_FileName(argv):
+    if len(argv) > 1:
+        return argv[1]
+    else:
+        return "bigTwitter.json"
 
 
-def order_hashtags():
-    pass
+# # count the total number of data records in a json lines file
+# def count_Records(tweet_file):
+#     count = 0
+#     jsonLines = json.loads(tweet_file.read())
+#     for line in jsonLines["rows"]:
+#         count += 1
+#     return count
 
 
 # Decide which grid box a given point with its x, y coordinates belongs to
 def which_grid_box(cor_x, cor_y, grid):
-    area = ""
+    area = None
     for box in grid:
         if cor_x >= box["xmin"] and cor_x <= box["xmax"] \
          and cor_y >= box["ymin"] and cor_y <= box["ymin"]:
@@ -50,133 +60,50 @@ def which_grid_box(cor_x, cor_y, grid):
     return area
 
 
-# def data_distribution(comm, grid, tweet_file, grid_dict, grid_hashtag_dict):
-#     with open(tweet_file, 'r') as f:
-#         tweet = json.loads(f.read())
-#         row_indicator = 0
-#         for row in tweet["rows"]:
-#             recipient = row_indicator % comm.Get_size()
-#
-#             print("row #", row_indicator, " to be send to ", recipient)
-#
-#             # comm.send(row, dest=recipient)
-#             row_indicator += 1
-#             process_tweet(comm, grid, grid_dict, grid_hashtag_dict,
-#                           row, recipient)
-
-def distribute_tweet(comm, tweet_file, size, grid, grid_dict, grid_hashtag_dict):
-    # rows_for_master = []
-    with open(tweet_file, 'r') as f:
-        tweet = json.loads(f.read())
-        row_indicator = 0
-        for row in tweet["rows"]:
-            row_indicator += 1
-            recipient = row_indicator % size
-            if row_indicator == 587:
-                print("row #587, recipient {} with \
-                      tweet row: {}".format(recipient, row))
-            print("row #", row_indicator, " to be send to ", recipient)
-            if comm.Get_rank() != recipient:
-                comm.send(row, dest=recipient)
-            else:
-                process_tweet(grid, row, grid_dict, grid_hashtag_dict)
-    comm.bcast("no more tweets", root=0)
-
-
-# def process_tweet(comm, grid, grid_dict, grid_hashtag_dict, row, recipient):
-#     # grid_dict = {}  # {"A1": num_tweets, "A2": num_tweets}
-#     # grid_hashtag_dict = {}
-#     # with open(tweet_file, 'r') as f:
-#     #     tweet = json.load(f)
-#     #     row_indicator = 0
-#     #     for row in tweet["rows"]:
-#     #         recipient = row_indicator % comm.Get_size()
-#     #         recvBuf = comm.sendrecv(row, dest=recipient)
-#     #         row_indicator += 1
-#     recvBuf = comm.Sendrecv(row, dest=recipient)
-#     print("Process #", comm.Get_rank(), "receives ", recvBuf)
-#     temp_cor_list = recvBuf["value"]["geometry"]["coordinates"]
-#     area = which_grid_box(temp_cor_list[0], temp_cor_list[1], grid)
-#     if area not in grid_dict.keys():
-#         if area != "":
-#             grid_dict[area] = 1
-#     else:
-#         grid_dict[area] += 1
-#     # code for find hashtag
-#     # ...
-#     grid_dict = comm.gather(grid_dict, root=0)
-#     return grid_dict, grid_hashtag_dict
-
-def has_more_tweets(recvBuf):
-    if type(recvBuf) == str and recvBuf == "no more tweets":
-        return False
-    return True
-
-
-def Get_tweet_row(comm, recvBuf):
-    recvBuf = comm.Recv(recvBuf, source=0)
-    return recvBuf
-
-
-def process_tweet(grid, tweet_row, grid_dict, grid_hashtag_dict):
-    temp_cor_list = tweet_row["value"]["geometry"]["coordinates"]
-    area = which_grid_box(temp_cor_list[0], temp_cor_list[1], grid)
-    if area not in grid_dict.keys():
-        if area != "":
-            grid_dict[area] = 1
-    else:
-        grid_dict[area] += 1
-    return grid_dict, grid_hashtag_dict
-
-
 def main(argv):
     comm = MPI.COMM_WORLD
-    # Get rank of process
     rank = comm.Get_rank()
-    processes = comm.Get_size()
-    grid = get_Grid(argv[0])  # suppose argv == "melbGrid.json"
+    size = comm.Get_size()
 
-    grid_dict = {}  # [{"A1":23, "B1":34},{}]
+    melbGrid = get_Grid(argv[0])
+    # print("grid:")
+    # for grid in melbGrid:
+    #     print(grid)
+
+    grid_cor_dict = {}
     grid_hashtag_dict = {}
 
-    if processes < 2:
-        # to be continue
-        pass
-    else:
-        if rank == 0:
-            print("now #0 starts data_distribution: ...\n")
-            distribute_tweet(comm, argv[1], processes, grid,
-                             grid_dict, grid_hashtag_dict)
-            # print("#0 start processes its own tweet record:...\n")
-            # grid_dict, grid_hashtag_dict = process_tweet(comm, grid)
-            grid_dict = comm.gather(grid_dict, root=0)
-        else:
-            while(True):
-                recvBuf = None
-                recvBuf = Get_tweet_row(comm, recvBuf)
-                if not has_more_tweets(recvBuf):
-                    break
-                print("#", comm.Get_rank(), " is in process")
+    if size > 1:
+        with open(argv[1], 'r') as f:
+            records = json.loads(f.read())["rows"]
+            num_records = len(records)
+            len_chunks = int(math.ceil(num_records / size))
+            start_Row = rank * len_chunks
+            if (rank + 1) * len_chunks > num_records:
+                grid_cor_dict, gird_hashtag_dict = doOperation_on_tweets(
+                    records[start_Row:], melbGrid)
+                print("Process #{}: with {}".format(rank, grid_cor_dict))
+                grid_cor_dict = comm.gather(grid_cor_dict, root=0)
+            else:
+                grid_cor_dict, gird_hashtag_dict = doOperation_on_tweets(
+                    records[start_Row:(start_Row + len_chunks)], melbGrid)
+                print("Process #{}: with {}".format(rank, grid_cor_dict))
+                grid_cor_dict = comm.gather(grid_cor_dict, root=0)
 
-                # keep updating its local dicts
-                grid_dict, grid_hashtag_dict = process_tweet(grid, recvBuf,
-                                                             grid_dict,
-                                                             grid_hashtag_dict)
-
-            grid_dict = comm.gather(grid_dict, root=0)
-    comm.barrier()
-    if rank == 0:
-        new_grid_dict = {}
-        for entry in grid_dict:
-            for k, v in entry.items():
-                if k not in new_grid_dict:
-                    new_grid_dict[k] = v
-                else:
-                    new_grid_dict[k] += v
-        print(new_grid_dict)
+            comm.barrier()
+            if rank == 0:
+                print(grid_cor_dict)
+                new_grid_dict = {}
+                for entry in grid_cor_dict:
+                    for k, v in entry.items():
+                        if k not in new_grid_dict:
+                            new_grid_dict[k] = v
+                        else:
+                            new_grid_dict[k] += v
+                print(new_grid_dict)
 
 
 if __name__ == "__main__":
     argv = ["/Users/jethrolong/Desktop/melbGrid.json",
-            "/Users/jethrolong/Desktop/tinyTwitter.json"]
-    main(argv)  # should be sys.argv later on
+            "/Users/jethrolong/Desktop/smallTwitter.json"]
+    main(argv)
