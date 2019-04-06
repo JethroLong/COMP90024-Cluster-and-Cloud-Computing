0feed450fe90c3a4e8e418ad19fe5b6e56e58be3
jethrolong <longj2@student.unimelb.edu.au>
2019-04-06 18:10:31 +1100

    boarder issues --- fixed

--- Assignment 1/src/ass1.py
+++ Assignment 1/src/ass1.py
@@ -94,12 +94,14 @@ def print_result(grid_dict, hashtag_dict, longest):
     print("===========================" * (longest + 1))
 
 
-# Decide which grid box a tweet belongs to with its x, y coordinates
+# Decide which grid box a tweet belongs to with its x, y coordinates.
+# Two classifiers ensure all tweets that exactly fall on grid boarders won't miss.
+# A tweet will be filtered out only when it fails both classification processes.
 def which_grid_box(cor_x, cor_y, grid):
-    for box in grid:
+    for box in grid:  # implement up-left rule to classify tweets
         if box["xmin"] < cor_x <= box["xmax"] and (box["ymin"] <= cor_y < box["ymax"]):
             return box["id"]
-    for box in grid:
+    for box in grid:  # For those who failed the first classifier, do second classification with down-right rule
         if box["xmin"] <= cor_x < box["xmax"] and (box["ymin"] < cor_y <= box["ymax"]):
             return box["id"]
     return None
